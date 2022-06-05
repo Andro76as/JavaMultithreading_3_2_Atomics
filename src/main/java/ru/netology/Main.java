2d797935a7700919f50aaf220fa6e14797ca8fb5
Andro76as <andro76as@gmail.com>
2022-06-05 18:07:30 +0300

    Commit

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,28 @@
+package ru.netology;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.LongAdder;
+
+public class Main {
+    private static final long SIZE = 20;
+    private static final int LOW_BOUND = 10;
+    private static final int HIGH_BOUND = 50;
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService =
+                Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        LongAdder stat = new LongAdder();
+        new Random().ints(SIZE, LOW_BOUND, HIGH_BOUND).boxed().toList()
+                .forEach(i -> executorService.submit(() -> stat.add(i)));
+        new Random().ints(SIZE, LOW_BOUND, HIGH_BOUND).boxed().toList()
+                .forEach(i -> executorService.submit(() -> stat.add(i)));
+        new Random().ints(SIZE, LOW_BOUND, HIGH_BOUND).boxed().toList()
+                .forEach(i -> executorService.submit(() -> stat.add(i)));
+        executorService.awaitTermination(3, TimeUnit.SECONDS);
+        System.out.println("Результат: " + stat.sum());
+        executorService.shutdown();
+    }
+}
